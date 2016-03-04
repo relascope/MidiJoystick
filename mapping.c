dd7eb2cc851498e9f2ec60723bf91e009e53c53f
osune <feldbischof@gmx.de>
2016-03-04 13:26:24 +0100

    housekeeping

--- mapping.c
+++ mapping.c
@@ -53,9 +53,6 @@ int get_midi_msg(struct js_event* ev, struct input_conf* input_map, size_t input
      axis = ev->value;
 
 
-
-     
-     
      /* transpose buttons to axis values */
      if (ev->type == JS_EVENT_BUTTON) {
 	  switch (axis) {
@@ -104,6 +101,8 @@ int get_midi_msg(struct js_event* ev, struct input_conf* input_map, size_t input
 	   create_midi_channel_pressure(im->chan, midi_val, midi);
 	   break;
      case (midi_command_note_on):
+
+	  /* change this to simple transpose */
 	  midi_val = midi_val - (AXIS_MIDI_NORM - im->param);
 	  create_midi_note_on(im->chan, midi_val, 0x7F, midi);
 	  break;
@@ -129,43 +128,3 @@ int get_midi_msg(struct js_event* ev, struct input_conf* input_map, size_t input
 }
 
 
-
-
-
- 
-#if 0
-int translate_input_to_midi(struct js_event* ev, struct midi_msg* midi)
-{
-     uint8_t lsb, msb;
-     long axis;
-     
-     if (ev->type & JS_EVENT_INIT)
-	  return -1;
-
-     if (ev->type == JS_EVENT_BUTTON) {
-	  fprintf(stderr, "No button actions defined\n");
-	  return -1;
-     }
-
-     
-     if (ev->type == JS_EVENT_AXIS) {
-	  /* create deadzone */
-	  if (ev->value < 4000 && ev->value > -4000)
-	       ev->value = 0;
-	  
-	  switch (ev->number) {
-	  case xbox_input_axis_y_left_as:
-	       axis = get_positive_axis(ev->value);
-	       map_to_midi_pitchbend_range(AXIS_POS_MIN, AXIS_POS_MAX, axis, &lsb, &msb);
-	       create_midi_pitch_bend(MIDI_CHANNEL, lsb, msb, midi);
-	       return 0;
-	       break;
-	  default:
-	       fprintf(stderr, "Axis number %d is not defined\n", ev->number);
-	       return -1;
-	       break;
-	  }
-     }
-     return 0;
-}
-#endif
