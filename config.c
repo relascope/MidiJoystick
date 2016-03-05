972e1f75fb03b19dbf85f487082ff9a917260072
osune <feldbischof@gmx.de>
2016-03-05 13:31:43 +0100

    configuration entries can be split across lines; configuration entries have to be delimited with a ';'

--- config.c
+++ config.c
@@ -328,20 +328,67 @@ void explain_configuration(struct input_conf* mapping, const size_t mapping_s)
 }
 
 
-static int read_line(char* buf, FILE *fp)
+
+
+
+static int read_till_sep(char *buf, FILE* fp, char sep, int* eof, int from)
 {
-     int c, i = 0;
-
-
-     while((buf[i++] = c = getc(fp)) != '\n' && i < CHAR_BUF_SIZE) {
-	  if (c == EOF)
-	       return c;
+     int c, i = from;
+     *eof = 0;
+     while ((buf[i++] = c = getc(fp)) != sep && i < CHAR_BUF_SIZE) {
+	  if (c == EOF) {
+	       *eof = 1;
+	       break;
+	  }
      }
      buf[--i] = '\0';
-     
-     fprintf(stderr,"CONF_LINE: %s\n", buf);
-
      return i;
+}
+
+
+static int read_config_line(char* buf, FILE *fp, int *eof)
+{
+     *eof = 0;
+     int n_pos = 0;
+     char *p;
+     while(!*eof) {
+	  if (!( strlen(buf) < CHAR_BUF_SIZE))
+	       return -1;
+	  
+	  read_till_sep(buf, fp, '\n', eof, n_pos);
+
+	  if (strchr(buf, '#') != NULL) {
+	       /* line contains comment -> skip */
+	       continue;
+	  }
+	  if ((p = strchr(buf, ';')) != NULL) {
+	       /* config seperator found -> done */
+	       *p = '\0';
+	       break;
+	  }
+	  n_pos = strlen(buf);
+
+     }
+
+     return strlen(buf);;
+}
+
+static int replace_char(char* buf, char* t, char n)
+{
+     char *p = NULL;
+     int cnt = 0;
+     for (int i = 0; i < strlen(t); ++i) {
+	  while((p = strchr(buf, t[i])) != NULL) {
+	       ++cnt;
+	       *p = n;
+	  }
+     }
+     return cnt;
+}
+
+static int multiline_to_singleline(char* buf)
+{
+     return replace_char(buf, "\n\t", ' ');
 }
 
 static int comp_input_conf_evid(const void* ev_id, const void* input_c)
@@ -361,35 +408,50 @@ int read_config_file(char* path, struct input_conf* mapping, size_t mapping_s)
 {
      FILE *fp;
      int eof_seen = 0;
-     int line_length, line_no;
+     int conf_length, n_lines, line_no;
      char buf[CHAR_BUF_SIZE];
      size_t map_idx = 0;
+     
 
      
      errno = 0;
-     if((fp = fopen(path, "r")) == NULL) {
+     if((fp = fopen(path, "rb")) == NULL) {
 	  perror("read_config_file");
 	  return -1;
      }
 
      init_config_parser();
 
-     line_no = 0;
+     line_no = 1;
      while(!eof_seen) {
+
 	  if(!(map_idx < mapping_s)) {
 	       fprintf(stderr,
 		       "CONFIG_WARN> joystick supports only %zu inputs, maybe some configuration is skipped\n",
 		       mapping_s);
 	       break;
 	  }
-	  ++line_no;
-	  if ((line_length = read_line(buf, fp)) == EOF) {
-	       eof_seen = 1;
-	       break;
-	  }
-
-	  if (line_length < 1 || buf[0] == '#')
+	  memset(buf, '\0', CHAR_BUF_SIZE);
+	  if((conf_length = read_config_line(buf, fp, &eof_seen)) < 1)
 	       continue;
+	  if(eof_seen)
+	       break;
+
+	  if (strchr(buf, '#') != NULL)
+	       continue;
+	  
+	  if ((n_lines = multiline_to_singleline(buf)) == 0)
+	       n_lines = 1;
+
+
+	  
+	  fprintf(stderr,"CONFIG_ENTRY %d: %s\n", line_no, buf);
+
+
+	  
+	  line_no += n_lines;	  
+
+
 
 	  memset(&mapping[map_idx], 0, sizeof(struct input_conf));
 	  if (parse_config_line(buf, &mapping[map_idx]) != 0){
