2dc23ffea9575707c0a0aec03c6d4b211cc74903
relascope <guru@DoJoy.at>
2023-10-10 22:53:45 +0200

    fix: read from /dev/input/js0

--- main.cpp
+++ main.cpp
@@ -12,12 +12,12 @@ int main() {
     std::cout << "MidiJoystick" << std::endl;
 
 
-    joystick_ js = open_joystick("/dev/input/event26");
-
-    js_event event;
-    memset(&event, 0, sizeof(joystick_));
+    joystick_ js = open_joystick("/dev/input/js0");
 
     while (true) {
+        js_event event;
+        memset(&event, 0, sizeof(joystick_));
+
         if (get_joystick_event(js, &event) != -1) {
             debug_print_joystick_event(&event);
         }
