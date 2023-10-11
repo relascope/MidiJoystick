4952958e36a9c0aebf787ed8a16c00d06f873880
relascope <guru@DoJoy.at>
2023-10-11 13:32:59 +0200

    feat: setup jack

--- midijack.h
+++ midijack.h
@@ -3,7 +3,16 @@
 
 #include <jack/ringbuffer.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 int setup_jack(jack_ringbuffer_t** ringbuf, size_t ringbuf_size );
 int tear_down_jack(void);
+
+#ifdef __cplusplus
+}
+#endif
+
 
 #endif
